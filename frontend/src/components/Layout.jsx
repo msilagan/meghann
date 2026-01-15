281012a262a1ab4ac03372dd7ff6300abca52334
Meghann Silagan <meghannsilagan@Meghanns-MacBook-Pro.local>
2026-01-14 16:38:23 -0800

    feaat: update background

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -9,7 +9,7 @@ export default function Layout({ children, darkMode, setDarkMode }) {
       className={`
     min-h-screen flex flex-col
     bg-gradient-to-br
-      from-indigo-50 via-white to-pink-100/75
+      from-indigo-100 via-white/20 to-pink-100/75
       dark:from-indigo-950 dark:via-gray-900 dark:to-pink-950
     transition-colors duration-700
     backdrop-blur-sm
